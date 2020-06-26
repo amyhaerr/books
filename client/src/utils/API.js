118aeb3af965c0113eaf48ca5766802e2ad0cbac
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-25 19:04:04 -0500

    work on API.js file

---
+++ client/src/utils/API.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default {
+    getBook: function (query) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+    },
+
+    // Deletes book with given id
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id).then(result => result.data);
+    },
+
+    // Saves a book to database
+    saveBook: function (bookData) {
+        return axios.post("/api/books", bookData).then(result => result.data);
+    },
+
+    // Get saved books from database
+    savedBooks: function() {
+        return axios.get("/api/books").then(result => result.data);
+    }
+}
