ff7bc1bbd27d3de8b45e49bc3610b946f099428c
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-30 10:17:30 -0500

    add build file

---
+++ client/build/precache-manifest.5ab39bfe235c8556cff558ec923bb32c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09a612fe1bb7994eab5f0941a8478cd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d139e4f3959200448eea",
+    "url": "/static/css/main.6552496e.chunk.css"
+  },
+  {
+    "revision": "ccb064ad0f660ab03a1c",
+    "url": "/static/js/2.54cdd6db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.54cdd6db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d139e4f3959200448eea",
+    "url": "/static/js/main.bf0b730c.chunk.js"
+  },
+  {
+    "revision": "f2e26c66aaf1664cdae8",
+    "url": "/static/js/runtime-main.3b6c25b6.js"
+  },
+  {
+    "revision": "1e065c659f1d6f3a4b0febb838ce314f",
+    "url": "/static/media/library.1e065c65.png"
+  }
+]);
