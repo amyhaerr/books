c7daf0989dbb681a9db6a2d1b5c4ea9dd789f525
Amy Haerr <amyhaerr@MacBook-Pro.local>
2020-06-30 20:09:39 -0500

    debug package.json server file

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -3,7 +3,7 @@ const db = require("../models");
 
 mongoose.connect(
     process.env.MONGODB_URI ||
-    "mongodb://localhoset/googlebooks"
+    "mongodb://localhost/googlebooks"
 );
 
 const bookSeed = 
